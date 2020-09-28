1d7edc33b9fe2b09e1888e28cba96001348065e2
Rituj Kumar <rituj.kumar@tesco.com>
2020-09-28 19:37:38 +0530

    removing the elastic dependency

--- src/main/java/com/rituj/elasticsearch/ElasticSearchApplication.java
+++ src/main/java/com/rituj/elasticsearch/ElasticSearchApplication.java
@@ -2,9 +2,7 @@ package com.rituj.elasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@EnableElasticsearchRepositories
 @SpringBootApplication
 public class ElasticSearchApplication {
 
